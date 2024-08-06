8b119a3ead69afb52ed7a9322861423ad3e7c53e
ks022564 <ks022564@gmail.com>
2024-08-06 12:28:39 +0900

    デプロイ修正６

--- db/migrate/20240805115341_remove_index_from_items_user_id.rb
+++ db/migrate/20240805115341_remove_index_from_items_user_id.rb
@@ -3,4 +3,5 @@ class RemoveIndexFromItemsUserId < ActiveRecord::Migration[7.0]
     if index_exists?(:items, :user_id, name: 'index_items_on_user_id')
       remove_index :items, name: 'index_items_on_user_id'
     end
+  end
 end
