8422ca52574651065b3d455b7e765222b9e06ac3
ks022564 <ks022564@gmail.com>
2024-08-06 12:24:39 +0900

    デプロイ修正５

--- db/migrate/20240805114609_add_foreign_key_to_items.rb
+++ db/migrate/20240805114609_add_foreign_key_to_items.rb
@@ -1,5 +1,22 @@
 class AddForeignKeyToItems < ActiveRecord::Migration[7.0]
-  def change
-    add_foreign_key :items, :users
+  def up
+    # 外部キー制約が存在しない場合のみ追加
+    unless foreign_key_exists?(:items, :users)
+      add_foreign_key :items, :users
+    end
+  end
+
+  def down
+    # 外部キー制約を削除
+    if foreign_key_exists?(:items, :users)
+      remove_foreign_key :items, :users
+    end
+  end
+
+  private
+
+  # 外部キー制約の存在をチェックするメソッド
+  def foreign_key_exists?(from_table, to_table)
+    foreign_keys(from_table).any? { |fk| fk.to_table == to_table.to_s }
   end
 end
